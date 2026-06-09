780ccbae6574a35b51655b9b7eb8534c5e78d763
ca-io-systems <systems@clientacquisition.io>
2026-06-09 14:09:50 -0400

    Property Cards Grid and Data

--- data/mockProperties.ts
+++ data/mockProperties.ts
@@ -1,13 +1,4 @@
-export interface Property {
-  id: number
-  title: string
-  type: string
-  location: string
-  price: number
-  rating: number
-  image: string
-  isSuperhost: boolean
-}
+import { Property } from '@/types/property'
 
 export const mockProperties: Property[] = [
   {
